12534dfbfc2b3acebe8a4dce657744c16322c020
Laurin Konzett <laurin.konzett@student.tugraz.at>
2025-12-18 11:00:40 +0100

    [INFO] automated commit [INFO]

--- main.c
+++ main.c
@@ -2,16 +2,5 @@
 
 int main(void)
 {
-  FILE* file = fopen("test.txt", "r");
-  if (file == NULL)
-  {
-    return 1;
-  }
-  int ch;
-  while ((ch = fgetc(file)) != EOF)
-  {
-    printf("%c", ch);
-  }
-  fclose(file);
   return 0;
 }
